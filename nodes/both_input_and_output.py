078df1bad5b9e70dea25c928cba73ec63c8977d0
buyalsky22 <burak.dursunlar@hotmail.com>
2020-03-18 21:46:29 +0300

    add excel loader and knn classifier

--- nodes/both_input_and_output.py
+++ nodes/both_input_and_output.py
@@ -1,8 +1,10 @@
-import sys, os
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+import os
+import sys
+
 import pandas as pd
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 
 sys.path.append(os.path.abspath(os.path.join("..")))
 from node import Node
@@ -26,7 +28,7 @@ class AttributeRemover(Node):
         self.dialog = QDialog()
         self.dialog.setWindowFlags(Qt.WindowTitleHint)
         self.setupUI(self.dialog)
-        print("ar" + self.df.columns)
+        print(self.df.columns)
         self.dialog.show()
 
     def setupUI(self, Dialog):
@@ -73,9 +75,9 @@ class AttributeRemover(Node):
     def remove_selected_columns(self):
         self.dialog.accept()
         dropped_columns = []
-        for checkbox in self.check_boxes:
+        for i, checkbox in enumerate(self.check_boxes):
             if checkbox.isChecked():
-                dropped_columns.append(checkbox.text())
+                dropped_columns.append(str(self.df.columns[i]))
                 print(checkbox.text())
         if dropped_columns:
             self.df.drop(dropped_columns, axis=1, inplace=True)
@@ -87,7 +89,11 @@ class AttributeRemover(Node):
         # order the nodes
         self.graphic_node.scene().scene.parent_widget.parent_window.order_path()
         # feed the next node
-        # self.graphic_node.scene().scene.parent_widget.parent_window.feed_next_node(self)
+        self.graphic_node.scene().scene.parent_widget.parent_window.feed_next_node(self)
+
+    @property
+    def output(self):
+        return self.df
 
     def feed(self, df):
         self.df = df
@@ -101,6 +107,131 @@ class NaiveBayesClassify(Node):
 class Knn(Node):
     def __init__(self, scene):
         super().__init__(scene, title="Knn", inputs=1, outputs=1)
+        self.df = None
+
+    def run(self):
+        if not isinstance(self.df, pd.core.frame.DataFrame):
+            QMessageBox.about(
+                self.scene.parent_widget,
+                "Warning!",
+                "You need to complete preceding nodes first."
+            )
+            return
+        self.dialog = QDialog()
+        self.dialog.setWindowFlags(Qt.WindowTitleHint)
+        self.setupUi(self.dialog)
+        print(self.df.columns)
+        self.dialog.show()
+
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(451, 205)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(Dialog.sizePolicy().hasHeightForWidth())
+        Dialog.setSizePolicy(sizePolicy)
+        self.buttonBox = QDialogButtonBox(Dialog)
+        self.buttonBox.setGeometry(QRect(100, 150, 341, 32))
+        self.buttonBox.setOrientation(Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.widget = QWidget(Dialog)
+        self.widget.setGeometry(QRect(20, 30, 401, 111))
+        self.widget.setObjectName("widget")
+        self.horizontalLayout = QHBoxLayout(self.widget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QLabel(self.widget)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
+        self.label_2 = QLabel(self.widget)
+        self.label_2.setObjectName("label_2")
+        self.verticalLayout.addWidget(self.label_2)
+        self.label_3 = QLabel(self.widget)
+        self.label_3.setObjectName("label_3")
+        self.verticalLayout.addWidget(self.label_3)
+        self.horizontalLayout.addLayout(self.verticalLayout)
+        self.verticalLayout_2 = QVBoxLayout()
+        self.verticalLayout_2.setObjectName("verticalLayout_2")
+        self.lineEdit = QLineEdit(self.widget)
+        #self.lineEdit.setValidator()
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout_2.addWidget(self.lineEdit)
+        self.comboBox = QComboBox(self.widget)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.comboBox.sizePolicy().hasHeightForWidth())
+        self.comboBox.setSizePolicy(sizePolicy)
+        self.comboBox.setObjectName("comboBox")
+
+        self.verticalLayout_2.addWidget(self.comboBox)
+        self.lineEdit_2 = QLineEdit(self.widget)
+        self.lineEdit_2.setValidator(QIntValidator(0, 100, self.lineEdit_2))
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.lineEdit_2.sizePolicy().hasHeightForWidth())
+        self.lineEdit_2.setSizePolicy(sizePolicy)
+        self.lineEdit_2.setObjectName("lineEdit_2")
+        self.verticalLayout_2.addWidget(self.lineEdit_2)
+        self.horizontalLayout.addLayout(self.verticalLayout_2)
+
+        self.retranslateUi(Dialog)
+        self.buttonBox.accepted.connect(self.apply_knn_classify)
+        self.buttonBox.rejected.connect(Dialog.reject)
+        QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.label.setText(_translate("Dialog", "Number of neighbors"))
+        self.label_2.setText(_translate("Dialog", "Target selection"))
+        self.label_3.setText(_translate("Dialog", "Test size percentage"))
+        for i in range(len(self.df.columns)):
+            self.comboBox.addItem(str(self.df.columns[i]))
+
+    def apply_knn_classify(self):
+        self.dialog.accept()
+
+        print("apply knn classify")
+        print(self.df.head())
+
+        self.split_target_and_inputs()
+        from sklearn.neighbors import KNeighborsClassifier
+        from sklearn.model_selection import train_test_split
+
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=int(self.lineEdit_2.text())/100, random_state=1, stratify=self.y)
+
+        knn = KNeighborsClassifier(n_neighbors=int(self.lineEdit.text()))
+        knn.fit(X_train, y_train)
+
+        print(knn.score(X_test, y_test))
+
+
+
+        self.is_finished = True
+        print("completed")
+        self.graphic_node.scene().scene.parent_widget.parent_window.change_statusbar_text()
+        # order the nodes
+        self.graphic_node.scene().scene.parent_widget.parent_window.order_path()
+        # feed the next node
+        #self.graphic_node.scene().scene.parent_widget.parent_window.feed_next_node(self)
+
+    def split_target_and_inputs(self):
+        self.X = self.df.drop(columns=[self.comboBox.currentText()])
+        self.y = self.df[self.comboBox.currentText()].values
+
+    def feed(self, df):
+        self.df = df
 
 
 class SVM(Node):
--- nodes/input_nodes.py
+++ nodes/input_nodes.py
@@ -125,12 +125,7 @@ class CsvLoader(Node):
         print("clicked")
         if not self.fname[0]:
             return
-        options = ["," if self.comboBox.currentText() == "Virgül" else ";",
-                   "," if self.comboBox_2.currentText() == "Virgül" else ".",
-                   "," if self.comboBox_3.currentText() == "Virgül" else ".", self.comboBox_4.currentText()]
-        self.df = pd.read_csv(self.fname[0], sep=options[0], thousands=options[1],
-                              decimal=options[2], encoding=options[3])
-        print(self.df.head(10))
+
 
     @property
     def output(self):
@@ -138,6 +133,12 @@ class CsvLoader(Node):
 
     def return_file(self):
         self.dialog.accept()
+        options = ["," if self.comboBox.currentText() == "Virgül" else ";",
+                   "," if self.comboBox_2.currentText() == "Virgül" else ".",
+                   "," if self.comboBox_3.currentText() == "Virgül" else ".", self.comboBox_4.currentText()]
+        self.df = pd.read_csv(self.fname[0], sep=options[0], thousands=options[1],
+                              decimal=options[2], encoding=options[3])
+        print(self.df.head(10))
         if isinstance(self.df, pd.core.frame.DataFrame):
             self.is_finished = True
             print("completed")
@@ -169,9 +170,9 @@ class ExcelLoader(Node):
 
     def setupUI(self, Dialog):
         Dialog.setObjectName("Dialog")
-        Dialog.resize(372, 324)
+        Dialog.resize(372, 134)
         self.buttonBox = QDialogButtonBox(Dialog)
-        self.buttonBox.setGeometry(QRect(10, 280, 341, 32))
+        self.buttonBox.setGeometry(QRect(10, 80, 341, 32))
         self.buttonBox.setOrientation(Qt.Horizontal)
         self.buttonBox.setStandardButtons(QDialogButtonBox.Cancel | QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
@@ -190,57 +191,6 @@ class ExcelLoader(Node):
         self.pushButton = QPushButton(self.widget)
         self.pushButton.setObjectName("pushButton")
         self.horizontalLayout.addWidget(self.pushButton)
-        self.widget1 = QWidget(Dialog)
-        self.widget1.setGeometry(QRect(20, 80, 331, 161))
-        self.widget1.setObjectName("widget1")
-        self.verticalLayout = QVBoxLayout(self.widget1)
-        self.verticalLayout.setContentsMargins(0, 0, 0, 0)
-        self.verticalLayout.setObjectName("verticalLayout")
-        self.horizontalLayout_2 = QHBoxLayout()
-        self.horizontalLayout_2.setObjectName("horizontalLayout_2")
-        self.label_2 = QLabel(self.widget1)
-        self.label_2.setObjectName("label_2")
-        self.horizontalLayout_2.addWidget(self.label_2)
-        self.comboBox = QComboBox(self.widget1)
-        self.comboBox.setObjectName("comboBox")
-        self.comboBox.addItem("")
-        self.comboBox.addItem("")
-        self.horizontalLayout_2.addWidget(self.comboBox)
-        self.verticalLayout.addLayout(self.horizontalLayout_2)
-        self.horizontalLayout_3 = QHBoxLayout()
-        self.horizontalLayout_3.setObjectName("horizontalLayout_3")
-        self.label_3 = QLabel(self.widget1)
-        self.label_3.setObjectName("label_3")
-        self.horizontalLayout_3.addWidget(self.label_3)
-        self.comboBox_2 = QComboBox(self.widget1)
-        self.comboBox_2.setObjectName("comboBox_2")
-        self.comboBox_2.addItem("")
-        self.comboBox_2.addItem("")
-        self.horizontalLayout_3.addWidget(self.comboBox_2)
-        self.verticalLayout.addLayout(self.horizontalLayout_3)
-        self.horizontalLayout_4 = QHBoxLayout()
-        self.horizontalLayout_4.setObjectName("horizontalLayout_4")
-        self.label_4 = QLabel(self.widget1)
-        self.label_4.setObjectName("label_4")
-        self.horizontalLayout_4.addWidget(self.label_4)
-        self.comboBox_3 = QComboBox(self.widget1)
-        self.comboBox_3.setObjectName("comboBox_3")
-        self.comboBox_3.addItem("")
-        self.comboBox_3.addItem("")
-        self.horizontalLayout_4.addWidget(self.comboBox_3)
-        self.verticalLayout.addLayout(self.horizontalLayout_4)
-        self.horizontalLayout_5 = QHBoxLayout()
-        self.horizontalLayout_5.setObjectName("horizontalLayout_5")
-        self.label_5 = QLabel(self.widget1)
-        self.label_5.setObjectName("label_5")
-        self.horizontalLayout_5.addWidget(self.label_5)
-        self.comboBox_4 = QComboBox(self.widget1)
-        self.comboBox_4.setObjectName("comboBox_4")
-        self.comboBox_4.addItem("")
-        self.comboBox_4.addItem("")
-        self.comboBox_4.addItem("")
-        self.horizontalLayout_5.addWidget(self.comboBox_4)
-        self.verticalLayout.addLayout(self.horizontalLayout_5)
 
         self.retranslateUi(Dialog)
         self.buttonBox.accepted.connect(self.return_file)
@@ -253,40 +203,35 @@ class ExcelLoader(Node):
         Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
         self.label.setText(_translate("Dialog", "Dosya seç"))
         self.pushButton.setText(_translate("Dialog", "Seç"))
-        self.label_2.setText(_translate("Dialog", "Ayraç"))
-        self.comboBox.setItemText(0, _translate("Dialog", "Virgül"))
-        self.comboBox.setItemText(1, _translate("Dialog", "Noktalı Virgül"))
-        self.label_3.setText(_translate("Dialog", "Bindelik ayracı"))
-        self.comboBox_2.setItemText(0, _translate("Dialog", "Virgül"))
-        self.comboBox_2.setItemText(1, _translate("Dialog", "Nokta"))
-        self.label_4.setText(_translate("Dialog", "Ondalık ayıracı"))
-        self.comboBox_3.setItemText(0, _translate("Dialog", "Nokta"))
-        self.comboBox_3.setItemText(1, _translate("Dialog", "Virgül"))
-        self.label_5.setText(_translate("Dialog", "Encoding"))
-        self.comboBox_4.setItemText(0, _translate("Dialog", "utf-8"))
-        self.comboBox_4.setItemText(1, _translate("Dialog", "utf-16"))
-        self.comboBox_4.setItemText(2, _translate("Dialog", "utf-32"))
+
 
     def file_select_clicked(self):
         print("clicked")
-        self.fname = QFileDialog.getOpenFileName(QWidget(), "Open File", "C:/", "Csv (*.csv)")
+        self.fname = QFileDialog.getOpenFileName(QWidget(), "Open File", "C:/", "Excel (*.xls *.xlsl)")
         print("clicked")
         if not self.fname[0]:
             return
-        options = ["," if self.comboBox.currentText() == "Virgül" else ";",
-                   "," if self.comboBox_2.currentText() == "Virgül" else ".",
-                   "," if self.comboBox_3.currentText() == "Virgül" else ".", self.comboBox_4.currentText()]
-        self.df = pd.read_csv(self.fname[0], sep=options[0], thousands=options[1],
-                              decimal=options[2], encoding=options[3])
+
+        self.df = pd.read_excel(self.fname[0])
         print(self.df.head(10))
 
     def return_file(self):
         self.dialog.accept()
+        print(type(self.df))
         if isinstance(self.df, pd.core.frame.DataFrame):
             self.is_finished = True
             print("completed")
+            self.graphic_node.scene().scene.parent_widget.parent_window.change_statusbar_text()
+            # order the nodes
+            self.graphic_node.scene().scene.parent_widget.parent_window.order_path()
+            # feed the next node
+            self.graphic_node.scene().scene.parent_widget.parent_window.feed_next_node(self)
         else:
             print("not completed")
+
+    @property
+    def output(self):
+        return self.df
 
     def evaluate(self):
         pass
