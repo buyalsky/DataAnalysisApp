357d5ab6bb22716825e06136ad8c4542d3c8fd63
Burak Dursunlar <burak.dursunlar@hotmail.com>
2020-07-25 16:10:19 +0300

    add menubar and docklayout tests

--- tests/test.py
+++ tests/test.py
@@ -3,11 +3,21 @@ import sys
 
 import pytest
 
+try:
+    from PyQt5.QtCore import *
+    from PyQt5.QtGui import *
+    from PyQt5.QtWidgets import *
+except ImportError:
+    from PySide2.QtCore import *
+    from PySide2.QtGui import *
+    from PySide2.QtWidgets import *
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
 
 import main_window
+from drag_list import NODES_TO_ICONS
 
 
 @pytest.fixture
@@ -19,9 +29,19 @@ def app(qtbot):
 
 
 class TestMainScreen:
-    def test1(self):
-        assert 2 + 2 == 4
-
     def test_statusbar(self, app):
         assert app.statusbar_label.text() == "0/0"
 
+    def test_menubar(self, app):
+        menu_list: [QMenu] = app.menuBar().findChildren(QMenu, "")
+        assert len(menu_list) == 2
+
+    def test_dock_widget(self, app):
+        dock = app.findChildren(QDockWidget, "")[0]
+        drag_lists = dock.findChildren(QListWidget, "")
+        assert len(drag_lists) == 7
+        for drag_list in drag_lists:
+            for i in range(drag_list.count()):
+                if "Demux" in drag_list.item(i).text():
+                    continue
+                assert NODES_TO_ICONS.get(drag_list.item(i).text())
